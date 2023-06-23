b2a5cbe0a53a494640f8caaf57a39eb8c2b734f0
Tejas N <maheshteju2002@gmail.com>
2023-06-24 00:57:36 +0530

    added schema's

---
+++ Server/models/PatentInfo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const patentInfoSchema = new mongoose.Schema({
+    patentApplicationId: {
+        type: String,
+        required: true
+    },
+    statusOfPatent: {
+        type: String,
+        required: true
+    },
+    inventorsName: {
+        type: String,
+        required: true
+    },
+    titleOfPatent: {
+        type: String,
+        required: true
+    },
+    applicantsNumber: {
+        type: String,
+        required: true
+    },
+    patentFilledDate: {
+        type: Date,
+        required: true
+    },
+    patentPublishedDate: {
+        type: Date,
+        required: true
+    },
+    patentGrantedDate: {
+        type: Date,
+    },
+    patentPublishedNumber: {
+        type: String,
+        required: true
+    },
+    patentGrantedNumber: {
+        type: String,
+    },
+    assigneeName: {
+        type: String,
+        required: true
+    },
+    mediaFile: {
+        type: String,
+    }
+});
+
+const PatentInfo = mongoose.model('Patent_Info', patentInfoSchema);
+
+module.exports = PatentInfo;
--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const XLSX = require('xlsx');
 
 const app = express();
 
 app.use(express.json());
 
+mongoose
+    .connect("mongodb://0.0.0.0:27017/Faculty-Data-Management-System", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("connection sucessfull!!"))
+    .catch((err) => console.log(err));
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
 
 app.listen(5500, () => {
     console.log('Server started on port 5500');
